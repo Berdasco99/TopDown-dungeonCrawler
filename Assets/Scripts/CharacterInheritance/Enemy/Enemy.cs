7c16e4c974078579d60152aa39f7bd8e114773e8
Berdasco99 <73401860+Berdasco99@users.noreply.github.com>
2024-05-09 01:19:34 +0200

    Añadi chasedistance
    Los enemigos ahora te persiguen solo si entras en el radio desde el que te pueden ver

--- Assets/Scripts/CharacterInheritance/Enemy/Enemy.cs
+++ Assets/Scripts/CharacterInheritance/Enemy/Enemy.cs
@@ -6,6 +6,7 @@ public class Enemy : Entity
 {
     protected GameObject player;
     protected float distance;
+    public float chaseDistance;
 
     protected void EnemyFunctions()
     {
@@ -18,18 +19,20 @@ public class Enemy : Entity
 
         Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
-
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+        if (distance < chaseDistance)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, moveSpeed * Time.deltaTime);
 
-        if(angle >= 90 && angle <= 180 || angle <= -90 && angle >= -180)
-        {
-            spriteRenderer.flipX = true;
-        }
-        else
-        {
-            spriteRenderer.flipX = false;
+            if (angle >= 90 && angle <= 180 || angle <= -90 && angle >= -180)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else
+            {
+                spriteRenderer.flipX = false;
+            }
         }
     }
 }
