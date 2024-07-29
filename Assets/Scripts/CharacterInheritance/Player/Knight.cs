50ed190eee835d3319e8746438703b5a042bbe2f
Berdasco99 <73401860+Berdasco99@users.noreply.github.com>
2024-07-29 15:38:39 +0200

    Dash works now
    Yippie

--- Assets/Scripts/CharacterInheritance/Player/Knight.cs
+++ Assets/Scripts/CharacterInheritance/Player/Knight.cs
@@ -7,10 +7,18 @@ public class Knight : Player
     [Header("KNIGHT ATTRIBUTES")]
     private bool canDash = true;
     private bool isDashing;
+    private GameObject player;
     public float dashingPower = 24f;
     public float dashingTime = 0.2f;
     public float dashingCD = 1f;
     public TrailRenderer trailRenderer;
+
+    private void Start()
+    {
+        player = this.gameObject;
+        Physics2D.IgnoreLayerCollision(8, 7, true);
+        Physics2D.IgnoreLayerCollision(8, 6, true);
+    }
 
     void Update()
     {
@@ -39,13 +47,14 @@ public class Knight : Player
     {
         canDash = false;
         isDashing = true;
-        boxCollider.enabled = false;
+        player.gameObject.layer = 8;
         rb.velocity = new Vector2(playerInput.x * dashingPower, playerInput.y * dashingPower);
         trailRenderer.emitting = true;
         yield return new WaitForSeconds(dashingTime);
         boxCollider.enabled = true;
         trailRenderer.emitting = false;
         isDashing = false;
+        player.gameObject.layer = 0;
         yield return new WaitForSeconds(dashingCD);
         canDash = true;
     }
